4b0983c4f1eac453d0bc50fe20ef1f68233eae16
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-04-06 18:20:35 +1000

    merged with cmanou's changes

--- app/settings.py
+++ app/settings.py
@@ -131,7 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    #'app.auth',
+    'app.auth',
     'app.website',
     'app.news',
     #'app.forms',
