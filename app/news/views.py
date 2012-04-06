3a78ee5a24d37ebc7311982a02ff314a50fdd1c4
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-04-06 17:42:47 +1000

    fixed for others

--- app/news/views.py
+++ app/news/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import get_object_or_404, render_to_response
 
 def feed(request):
     news = Item.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('partials/news/feed.html', {'news': news} )
+    return render_to_response('news/feed.html', {'news': news} )
