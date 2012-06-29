15bb59cfbf4415e2547af2dd1e381bd197a28d46
Christopher Manouvrier <cmanou@mac.com>
2012-06-29 11:33:48 +1000

    Fixed Sponsors and got rid of wysiwyg

--- app/website/admin.py
+++ app/website/admin.py
@@ -2,35 +2,35 @@ from app.website.models import *
 from django.contrib import admin
 
 
-# we define our resources to add to admin pages
-class CommonMedia:
-  js = (
-	'https://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojo/dojo.js',
-	'/assets/js/editor.js',
-  )
-  css = {
-	'all': ('/assets/css/editor.css',),
-  }
+# # we define our resources to add to admin pages
+# class CommonMedia:
+#   js = (
+# 	'https://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojo/dojo.js',
+# 	'/assets/js/editor.js',
+#   )
+#   css = {
+# 	'all': ('/assets/css/editor.css',),
+#   }
+# 
+# # let's add it to this model
+# admin.site.register(About,
+#   Media = CommonMedia,
+# )
+# admin.site.register(FunStuff,
+#   Media = CommonMedia,
+# )
+# admin.site.register(Team,
+#   Media = CommonMedia,
+# )
+# admin.site.register(Slug,
+#   Media = CommonMedia,
+# )
+# admin.site.register(Sponsor,
+#   Media = CommonMedia,
+# )
 
-# let's add it to this model
-admin.site.register(About,
-  Media = CommonMedia,
-)
-admin.site.register(FunStuff,
-  Media = CommonMedia,
-)
-admin.site.register(Team,
-  Media = CommonMedia,
-)
-admin.site.register(Slug,
-  Media = CommonMedia,
-)
-admin.site.register(Sponsor,
-  Media = CommonMedia,
-)
-
-# admin.site.register(About)
-# admin.site.register(FunStuff)
-# admin.site.register(Team)
-# admin.site.register(Slug)
-# admin.site.register(Sponsor)
+admin.site.register(About)
+admin.site.register(FunStuff)
+admin.site.register(Team)
+admin.site.register(Slug)
+admin.site.register(Sponsor)
