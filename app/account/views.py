34e096ced118888dee5a1049af3577f90a76538f
Christopher Manouvrier <cmanou@mac.com>
2012-07-16 17:50:27 +1000

    Fix mailing direct

--- app/account/views.py
+++ app/account/views.py
@@ -16,7 +16,7 @@ def view(request):
 def update_mailing(request):
    if request.user.is_authenticated():
        h = httplib.HTTPConnection('cgi.cse.unsw.edu.au')
-       h.request('GET', '/~csesoc/mailingLists?cseid=' + request.user.get_profile().cselogin)	
+       h.request('GET', '/~csesoc/mailingLists?cseid=' + request.user.profile.cselogin)	
        cse = h.getresponse().read()
        teams = cse.split(',')
        return render_to_response('account/mailing.html', {'teams': teams}, context_instance=RequestContext(request))
