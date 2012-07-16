483bac2cb0180aecf0af34d1d7d5def860f85fab
Christopher Manouvrier <cmanou@mac.com>
2012-07-16 17:06:13 +1000

    Revert "fakeroot profile for dev"
    This reverts commit d83324ed476bbe5c6c343a9789db08187cd0171c.

--- app/account/models.py
+++ app/account/models.py
@@ -1,26 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
-from django import forms
-from django.conf import settings
-
 import httplib
+from django import forms
+
 
 class UserProfile(models.Model):
-   cselogin = models.CharField(max_length=200)
-   user = models.ForeignKey(User, unique=True)
-   def __unicode__(self):
-      return self.cselogin
+	cselogin = models.CharField(max_length=200)
+	user = models.ForeignKey(User, unique=True)
+	def __unicode__(self):
+		return self.cselogin
 
-   def get_or_create_profile(self, user):
-      if settings.DEBUG:
-         cse = 'fakeroot'
-      else:
-         h1 = httplib.HTTPConnection('cgi.cse.unsw.edu.au')
-         h1.request('GET', '/~samli/cseid.cgi?id=' + user.username)	
-         cse = h1.getresponse().read()
-      user.profile, c = UserProfile.objects.get_or_create(user=user,cselogin=cse)
-      return user.profile
-
-   User.profile = property(get_or_create_profile)
+	def get_or_create_profile(user):
+		h1 = httplib.HTTPConnection('cgi.cse.unsw.edu.au')
+		h1.request('GET', '/~samli/cseid.cgi?id=' + user.username)	
+		cse = h1.getresponse().read()
+		user.profile, c = UserProfile.objects.get_or_create(user=user,cselogin=cse)
+		return user.profile
+		
+	User.profile = property(get_or_create_profile)
