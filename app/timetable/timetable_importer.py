569f4b911aa61592e57924e5e9fbc5c793d77b18
Christopher Lam <calamity@chromium.org>
2013-04-08 21:56:53 +1000

    update timetable importer
    use google api v3
    use OAuth for google auth
    fix modal dialog to be slower (like the new importer!)

--- app/timetable/timetable_importer.py
+++ app/timetable/timetable_importer.py
@@ -5,40 +5,23 @@ try:
   from xml.etree import ElementTree # for Python 2.5 users
 except ImportError:
   from elementtree import ElementTree
-import gdata.service
-import atom.service
-import gdata.calendar
-import gdata.calendar.service
-import atom
 
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import run
+
+import httplib2
 from BeautifulSoup import BeautifulSoup
 import re
-from datetime import date
-from datetime import timedelta
 import urllib2
 import cookielib
 import re
 import urllib
-import socket
 
-import time
 import datetime
-
-
-#THIS IS STUPID. PYTZ SUCKS PENIS. UGHHH
-dst_dates = {
-   2011:['3/4/2011','2/10/2011'],
-   2012:['1/4/2012','7/10/2012'],
-   2013:['7/4/2013','6/10/2013'],
-   2014:['6/4/2014','5/10/2014'],
-   2015:['5/4/2015','4/10/2015'],
-   2016:['3/4/2016','2/10/2016'],
-   2017:['2/4/2017','1/10/2017'],
-   2018:['1/4/2018','7/10/2018'],
-   2019:['7/4/2019','6/10/2019'],
-   2020:['5/4/2020','4/10/2020'],
-   2021:['4/4/2021','3/10/2021']
-}
 
 unsw_start_dates = {
    '11s1':'28/2/2011',
@@ -61,167 +44,145 @@ login_url='https://ssologin.unsw.edu.au/cas/login?service=https%3A%2F%2Fmy.unsw.
 
 timetable_url='https://my.unsw.edu.au/active/studentTimetable/timetable.xml'
 
+def getFlow(full_path):
+  print full_path
+  return OAuth2WebServerFlow(client_id='216834235087@developer.gserviceaccount.com',
+                             client_secret='afPuuycPMDAcEGKgGvpF6c-9',
+                             scope='https://www.googleapis.com/auth/calendar',
+                             redirect_uri=full_path)
+
+def getGoogleRedirect(full_path):
+  return getFlow(full_path).step1_get_authorize_url()
+
 def getTimetable(zUser, zPass):
-    jar = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
-    stupid_thing = re.findall(r'_cNoOpConversation.*?"', opener.open(login_url).read())[0].replace('"', '')
-    data = {'username':zUser, 'password':zPass, '_eventId':'submit', 'lt':stupid_thing}
-    opener.open(login_url, urllib.urlencode(data))
-    return opener.open(timetable_url).read()
+  jar = cookielib.CookieJar()
+  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+  # CSRF Token or something. We need to steal it from SSO
+  # into our cookie jar to go open the timetable page
+  stupid_thing = re.findall(r'_cNoOpConversation.*?"', opener.open(login_url).read())[0].replace('"', '')
+  data = {'username': zUser, 'password': zPass, '_eventId': 'submit', 'lt': stupid_thing}
+  opener.open(login_url, urllib.urlencode(data))
+  return opener.open(timetable_url).read()
 
 def CreateClassEvent(title, content, where, start_time, end_time):
-    event = gdata.calendar.CalendarEventEntry()
-    event.title = atom.Title(text=title)
-    event.content = atom.Content(text=content)
-    event.where.append(gdata.calendar.Where(value_string=where))
-    event.when.append(gdata.calendar.When(start_time=start_time, end_time=end_time))
-    event.batch_id = gdata.BatchId(text='insert-request')
-    return event
+  event = {
+      'summary': title,
+      'description': content,
+      'location': where,
+      'start': {
+        'dateTime' : start_time,
+        'timeZone': 'Australia/Sydney',
+      },
+      'end': {
+        'dateTime': end_time,
+        'timeZone': 'Australia/Sydney',
+      }
+    }
+  return event
 
-def dateCompare(date1, date2):
-    if date1.year < date2.year:
-        return -1
-    elif date1.year == date2.year:
-         if date1.month < date2.month:
-            return -1
-         elif date1.month == date2.month:
-            if date1.day < date2.day:
-               return -1
-            elif date1.day == date2.day:
-               return 0
-            else:
-               return 1
-         else:
-            return 1
-    else:
-         return 1
+def export(f, source, zu, zp, code, full_path):
+  if f == 'use-login' and (not zu or not zp):
+    return "No zPass details or timetable source"
 
-def export(f, gu, gp, zu, zp):
-   if not gp or not gu:
-       return "Missing Google username and password - if you don't want to upload direct to gcal, use http://www.cse.unsw.edu.au/~szyf396/timetable.html instead"
+  if f == 'use-login':
+    print "getting timetable"
+    f = getTimetable(zu, zp)
+    print "got timetable!"
+  else:
+    f = source
 
-   if f == 'use-login' and (not zu or not zp):
-       return "No zPass details or timetable source"
+  if "sectionHeading" not in f:
+    return "Bad timetable source, possibly incorrect login details or myunsw daily dose of downtime (12am-2am or whatever)"
 
-   if f == 'use-login':
-       print "getting timetable"
-       f = getTimetable(zu, zp)
-       print "got timetable!"
+  # parsing shit
+  s = BeautifulSoup(f.replace("\n",""))
 
-   if "sectionHeading" not in f:
-       return "Bad timetable source, possibly incorrect login details or dipshit myunsw general incompetence downtime (12am-2am or whatever)"
+  credentials = getFlow(full_path).step2_exchange(code)
+  http = httplib2.Http()
+  http = credentials.authorize(http)
 
-   # parsing shit
-   s = BeautifulSoup(f.replace("\n",""))
+  service = build('calendar', 'v3', http=http)
 
-   calendar_service = gdata.calendar.service.CalendarService()
-   calendar_service.email = gu
-   calendar_service.password = gp
+  ####################################################
+  #  LOOK EVERYONE, I'M THROWING YOUR PASSWORDS AWAY #
+  ####################################################
+  zp = ''
 
-   #logging into google
-   try:
-      calendar_service.ProgrammaticLogin()
-   except socket.sslerror:
-      return "Probably out of quota. Wait until quota resets (socket.sslerror)"
-   except gdata.service.BadAuthentication:
-      return "Bad groogle username or password"
-
-   ####################################################
-   #  LOOK EVERYONE, I'M THROWING YOUR PASSWORDS AWAY #
-   ####################################################
-   zp = ''
-   gp = ''
-
-   name = re.sub('@.*', '', gu)
-   sem = re.sub(u'.*Semester (\S+) \S\S(\S+).*', u'\\2s\\1', s.find("option", {'selected':'true'}).text)
-   title = name + "'s " + sem + " Timetable"
+  sem = re.sub(u'.*Semester (\S+) \S\S(\S+).*', u'\\2s\\1', s.find("option", {'selected':'true'}).text)
+  title = sem + " Timetable"
 
 
-   # make gcal calendar
-   calendar = gdata.calendar.CalendarListEntry()
-   calendar.title = atom.Title(text=title)
-   calendar.hidden = gdata.calendar.Hidden(value='false')
-   calendar.timezone = gdata.calendar.Timezone(value='Australia/Sydney')
+  # make gcal calendar
+  calendar = {
+      'summary': title,
+      'timeZone': 'Australia/Sydney',
+    }
 
-   try:
-      calendar = calendar_service.InsertCalendar(new_calendar=calendar)
-   except gdata.calendar.service.RequestError:
-      return "Failed to create new calendar " + title
+  created_calendar = service.calendars().insert(body=calendar).execute()
+  print created_calendar['id']
 
+  week_after_midsem_break = int(s.find(text="N1").findNext("table").findNext("td").text)
 
-   batch_request_feed = gdata.calendar.CalendarEventFeed()
+  courses = [x.contents[0] for x in s.findAll("td", {"class":"sectionHeading"})]
 
-   week_after_midsem_break = int(s.find(text="N1").findNext("table").findNext("td").text)
+  print "Parsing calendar to make events"
 
-   courses = [x.contents[0] for x in s.findAll("td", {"class":"sectionHeading"})]
+  for course in courses:
+    # FINGERS CROSSED THAT THE TIMETABLE PAGE NEVER CHANGES
+    classes = s.find(text=course).findNext("table").findAll("tr", {"class": re.compile("data")})
+    ctype, code, day, tim, weeks, place, t = ['' for x in xrange(7)]
+    for c in classes:
+      a = [(x.contents[0] if x.contents else "") for x in c.findAll("td", recursive=False)]
+      g = (t for t in a)
 
-   print "Parsing calendar to make events"
+      t = g.next()
 
-   for course in courses:
-       classes = s.find(text=course).findNext("table").findAll("tr", {"class":re.compile("data")})
-       ctype, code, day, tim, weeks, place, t = ['' for x in xrange(7)]
-       for c in classes:
-           a = [(x.contents[0] if x.contents else "") for x in c.findAll("td", recursive=False)]
-           g = (t for t in a)
-        
-           t = g.next()
+      if t.strip() != "&nbsp;":
+        ctype = t
 
-           if t.strip() != "&nbsp;":
-               ctype = t
+      t = g.next()
+      if t.strip() not in days:
+        code = t
+        day = g.next().strip()
+      else:
+        day = t.strip()
+      tim = g.next()
+      weeks = g.next()
+      place = g.next()
+      t = ' '.join(g.next().findAll(text=True))
 
-           t = g.next()
-           if t.strip() not in days:
-               code = t
-               day = g.next().strip()
-           else:
-               day = t.strip()
-           tim = g.next()
-           weeks = g.next()
-           place = g.next()
-           t = ' '.join(g.next().findAll(text=True))
+      if tim.find(" - ") == -1:
+        continue
+      start, end = tim.split(" - ")
+      start = datetime.datetime.strptime(unsw_start_dates[sem] + ' ' + start, "%d/%m/%Y %I:%M%p")
+      end = datetime.datetime.strptime(unsw_start_dates[sem] + ' ' + end, "%d/%m/%Y %I:%M%p")
 
-           if tim.find(" - ") == -1:
-               continue
-           start, end = tim.split(" - ")
-           start = datetime.datetime.strptime(unsw_start_dates[sem] + ' ' + start,"%d/%m/%Y %I:%M%p")
-           end = datetime.datetime.strptime(unsw_start_dates[sem] + ' ' + end,"%d/%m/%Y %I:%M%p")        
-   
-           course = course.split()[0]
-        
+      course = course.split()[0]
 
-           w = []
-           for r in weeks.split(","):
-               if "N" in r:
-                   print "Did not process %s %s %s because of non-integer week"
-                   continue
-               if "-" in r:
-                   w+=range(int(r.split("-")[0]), int(r.split("-")[1])+1)
-               else:
-                   w+=[int(r)]
-                
-           weeks = w
-        
-           for w in weeks:
-               if w < week_after_midsem_break:
-                   w -= 1
-               thisStart = start + datetime.timedelta(7 * w + days[day])
-               thisEnd = end + datetime.timedelta(7 * w + days[day])
-            
-               fmt = "%d/%m/%Y"
-               if dateCompare(thisStart, datetime.datetime.strptime(dst_dates[start.year][0], fmt)) >= 0 and dateCompare(thisStart, datetime.datetime.strptime(dst_dates[start.year][1], fmt)) < 0:
-                  tzdelta = -10
-               else:
-                  tzdelta = -11
+      w = []
+      for r in weeks.split(","):
+        if "N" in r:
+          print "Did not process %s %s %s because of non-integer week"
+          continue
+        if "-" in r:
+          w += range(int(r.split("-")[0]), int(r.split("-")[1])+1)
+        else:
+          w += [int(r)]
+          
+      weeks = w
 
-               thisStart += datetime.timedelta(hours=tzdelta)
-               thisEnd += datetime.timedelta(hours=tzdelta)
-               thisEvent = CreateClassEvent('%s %s' % (course, ctype), "Instructor: %s Code:%s"%(t.strip(), code), place, thisStart.strftime('%Y-%m-%dT%H:%M:%S.000Z'), thisEnd.strftime('%Y-%m-%dT%H:%M:%S.000Z'))
-               batch_request_feed.AddInsert(entry=thisEvent)            
+      for w in weeks:
+        if w < week_after_midsem_break:
+          w -= 1
+        thisStart = start + datetime.timedelta(7 * w + days[day])
+        thisEnd = end + datetime.timedelta(7 * w + days[day])
 
-   print "Uploading events"
-   try:
-      calendar_service.ExecuteBatch(batch_request_feed, 'https://www.google.com/calendar/feeds/' + re.sub('.*/', '', calendar.id.text) + '/private/full/batch')
-   except gdata.calendar.service.RequestError:
-      return "Error. Most likely a google api failure. Try again. If it happens lots, raise a bug"
-   print "Probably success!"
+        thisEvent = CreateClassEvent('%s %s' % (course, ctype),
+                                     "Instructor: %s Code:%s" % (t.strip(), code),
+                                     place,
+                                     thisStart.strftime('%Y-%m-%dT%H:%M:%S.000'),
+                                     thisEnd.strftime('%Y-%m-%dT%H:%M:%S.000'))
+        service.events().insert(calendarId=created_calendar['id'], body=thisEvent).execute()
+
+  print "Probably success!"
 
