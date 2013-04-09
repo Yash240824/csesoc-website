719ff074a5f73041a4cb0d86741b9050c5dd553f
Samuel Li <samli@codesphere.com>
2013-04-09 15:59:42 +1000

    [hs] Fixed the submit registration page. Updated the thanks page. Input validation on forms.

--- app/hs/views.py
+++ app/hs/views.py
@@ -7,7 +7,6 @@ from django.forms import ModelForm,Textarea
 class RegistrationForm(ModelForm):
    class Meta:
       model = Registration
-      exclude = ('course')
 
 def main(request):
     courses = Course.objects.all()
@@ -17,8 +16,11 @@ def signup(request):
   if request.method == 'POST': # form submitted
     form = RegistrationForm(request.POST) # form bound to POST data
     if form.is_valid():
+      # Send automated email to intro to programming
+      if request.POST.get('course') is 1:
+        send_mail("hello", "message", "csesoc.compclub@cse.unsw.edu.au", "samli@codesphere.com")
       form.save() # create new Application instance
-      return render_to_response('hs/thanks.html', context_instance=RequestContext(request))
+      return render_to_response('hs/thanks.html', {'email' : request.POST.get('email')},context_instance=RequestContext(request))
     else:
       course = Course.objects.get(id=request.POST.get('course'))
       return render_to_response('hs/signup.html', {'form' : form, 'c': course}, context_instance=RequestContext(request))
--- public/assets/js/hs.js
+++ public/assets/js/hs.js
@@ -21,4 +21,23 @@ $(document).ready(function () {
 			return false;
 		}
 	});
+
+	// Validate rego
+	$("#submitRego").click(function () {
+		valid = true;
+		$.each($('input[type=text]'), function(i, v) {
+			if (v.value == '') {
+				$('#regoError').slideDown();
+				$(v).parent().addClass('error');
+				valid = false;
+			} else {
+				$(v).parent().removeClass('error');
+			}
+		});
+		// scroll to the top so they can see the error message
+		if (!valid) {
+			$("html, body").animate({ scrollTop: 0 }, 600);
+		}
+		return valid;
+	});
 });
