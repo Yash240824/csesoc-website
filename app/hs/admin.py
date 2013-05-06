c30870a95f615bfafa32e18ead5bcf7df0ca9a0c
Samuel Li <samli@codesphere.com>
2013-05-06 15:10:28 +1000

    Changed registration admin panel for HS.

--- app/hs/admin.py
+++ app/hs/admin.py
@@ -1,5 +1,10 @@
 from app.hs.models import *
 from django.contrib import admin
 
+class RegistrationAdmin(admin.ModelAdmin):
+  list_filter = ('course', 'gender')
+  list_display = ('full_name', 'email', 'level', 'gender', 'highschool', 'laptop')
+  #actions = []
+
 admin.site.register(Course)
-admin.site.register(Registration)
+admin.site.register(Registration, RegistrationAdmin)
