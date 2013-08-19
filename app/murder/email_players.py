51fb37c8a41e4aa5b5a5426048f3d9422559a750
Matthew Moss <mdm@cse.unsw.edu.au>
2013-08-19 23:40:34 +1000

    Fix email bugs for murder

--- app/murder/email_players.py
+++ app/murder/email_players.py
@@ -19,15 +19,19 @@ from app.murder.models import *
 
 current_round = Round.objects.order_by('-end')[0]
 players = RoundPlayer.objects.filter(round=current_round)
+test_mode = False
+
+if (len(sys.argv) > 2):
+    print "Test mode"
+    test_mode = True
 
 for p in players:
     email = User.objects.filter(username=p.player.username)[0].email
     message = render_to_string('murder/email/newround.txt', {'rp':p})
-    if (len(sys.argv) > 2):
-        print "Test mode"
-        email = "csesoc.dev.head@cse.unsw.edu.au"
     if (len(sys.argv) > 1):
         message = sys.argv[1] + '\n' + message
-    send_mail('Welcome to Murder@CSE', message, 'csesoc.dev.murder@cse.unsw.edu.au', [email], fail_silently=True)
-    if (sys.argv > 2):
-        sys.exit()
+    print "Sending", p
+    if not test_mode:
+        send_mail('Welcome to Murder@CSE', message, 'csesoc.dev.murder@cse.unsw.edu.au', [email], fail_silently=True)
+    else:
+        send_mail('Welcome to Murder@CSE', message, 'csesoc.dev.murder@cse.unsw.edu.au', ["csesoc.dev.head@cse.unsw.edu.au"], fail_silently=True)
