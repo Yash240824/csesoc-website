fd271a76b8b401733b232e905348991b4070af39
Matthew Moss <mdm@cse.unsw.edu.au>
2013-10-14 12:39:36 +1100

    Update murder to fix missing new round bugs

--- app/murder/new_week.py
+++ app/murder/new_week.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Begin dirty path hack
 import os
 from os import path
@@ -25,20 +24,33 @@ if currentGames.count() > 0:
   difference = date.today() - currentGame.start_day
   roundname = "Week %d" % (difference.days / 7)
 
-  # Set the round start and end dates
-  roundlength = timedelta(days=7) - timedelta(minutes=20)
-  if len(sys.argv) > 1 and sys.argv[1] == "--short":
-      roundlength = timedelta(days=2) - timedelta(minutes=20)
   startdate = datetime.now()
+  """
+  The round length should be:
+    (7 days - 20 minutes) if there's room before the end of the game
+    (end of game)         if there's not
+    (2 days - 20 minutes) if the --short option is used
+  """
+
+  if len(sys.argv) > 1 and sys.argv[1] == "--short":
+    roundlength = timedelta(days=2) - timedelta(minutes=20)
+  else:
+    roundlength = timedelta(days=7) - timedelta(minutes=20)
+
   enddate = startdate + roundlength
+  if enddate.date() >= currentGame.last_day:
+    enddate = currentGame.end - timedelta(minutes=20)
 
   r = Round(name=roundname, start=startdate, end=enddate, game=currentGame)
   r.save()
 
-current_round = Round.objects.order_by('-end')[0]
-players = RoundPlayer.objects.filter(round=current_round)
+  current_round = Round.objects.order_by('-end')[0]
+  players = RoundPlayer.objects.filter(round=current_round)
 
-for p in players:
+  for p in players:
     email = User.objects.filter(username=p.player.username)[0].email
     message = render_to_string('murder/email/newround.txt', {'rp':p})
     send_mail('Welcome to Murder@CSE', message, 'csesoc.dev.murder@cse.unsw.edu.au', [email], fail_silently=True)
+
+else:
+  send_mail('Murder@CSE round failure', "No current game found", 'csesoc.dev.murder@cse.unsw.edu.au', ["csesoc.dev.head@cse.unsw.edu.au"], fail_silently=False)
