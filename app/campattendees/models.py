0bf178a96b2066ef3badb1657f69a4ff4a30307c
vincentt143 <tran.vincent17@gmail.com>
2014-02-21 00:03:48 +1100

    Payment for Camp 2014

--- app/campattendees/models.py
+++ app/campattendees/models.py
@@ -7,6 +7,7 @@ class Application(models.Model):
    student_number = models.CharField(max_length=8)
    contact_number = models.CharField(max_length=15)
    gender = models.CharField(max_length=1, choices=campglobals.GENDER_CHOICES)
+   arc = models.BooleanField(default=False)
    cse_program = models.CharField(max_length=2, choices=campglobals.PROGRAM_CHOICES, verbose_name='CSE program')
 
    age = models.DateField(verbose_name='Date of Birth', help_text='Proof of age will be required if you wish to consume alcohol.')
@@ -14,7 +15,7 @@ class Application(models.Model):
    medical = models.TextField(help_text='Please list any medical conditions that should be disclosed above.', blank=True)
 
    payment_status = models.CharField(max_length=1, choices=campglobals.PAYMENT_CHOICES, default='N')
-   medical_pdf = models.FileField(upload_to="storage/uploads", blank=True, null=True)
+   medical_pdf = models.FileField(upload_to="storage/uploads", blank=True)
    year = models.IntegerField(verbose_name='Application Year', editable=False)
    shirt_size = models.CharField(max_length=3, choices=campglobals.SHIRT_CHOICES, default='N')
 
--- app/campattendees/views.py
+++ app/campattendees/views.py
@@ -17,21 +17,29 @@ def signup(request):
       this_year = datetime.date.today().year
       if request.method == 'POST': # form submitted
          student = Application.objects.filter(student_number=request.user.username)
+         deposit = False
          if len(student) == 0:
             student = Application(year=this_year)
          else:
             student = student[0]
+            if student.payment_status == "D":
+             deposit = True
+         arc = False
+         if request.POST['arc']:
+             arc = True
          form = ApplicationForm(request.POST, request.FILES, instance=student) # form bound to POST data
+         early_bird = False
+         if str(datetime.date.today()) < "2014-03-07":
+             early_bird = True
          if form.is_valid():
             form.save()
-            return render_to_response('camp/thanks-signup.html', context_instance=RequestContext(request))
+            return render_to_response('camp/thanks-signup.html', {'arc': arc, 'early_bird':  early_bird, 'deposit': deposit}, context_instance=RequestContext(request))
       else:
          student = Application.objects.filter(student_number=request.user.username)
          if len(student) == 0:
              appl = Application(year=this_year, student_number = request.user.username)
              form = ApplicationForm(instance=appl) # unbound form
          else:
-             print student[0]
              form = ApplicationForm(instance=student[0]) # unbound form
       return render_to_response('camp/signup.html', {'form' : form}, context_instance=RequestContext(request))
    else:
